31b32b9db640f9511e9b3ac2d8fc7a7ec94eeaf6
wgregoryii <90974249+wgregoryii@users.noreply.github.com>
2021-09-30 12:38:03 -0500

    Test 7

--- flask-01-02-hello-world-app-Jinja_Template/flask-02-Jinja_Template/jinja.py
+++ flask-01-02-hello-world-app-Jinja_Template/flask-02-Jinja_Template/jinja.py
@@ -7,7 +7,7 @@ def head():
     return render_template("index.html", number1=34, number2=15)
     
 
-@app.route("/Karil")
+@app.route("/karil")
 def number():
     num1 , num2 = 23, 45
     return render_template("body.html", value1=num1, value2=num2, value3=num1+num2)
