59cac0992a6c3245df2d8b70863a2630ecb31943
wgregoryii <90974249+wgregoryii@users.noreply.github.com>
2021-10-06 20:26:54 -0500

    Commit

---
+++ flask-03-handling-routes-and-if-for/Flask_if_for_structure/app.py
@@ -0,0 +1,11 @@
+# Import Flask modules
+
+# Create an object named app 
+
+# Create a function named head which shows the massage as "This is my first conditions experience" in `index.html` 
+# and assign to the route of ('/')
+
+# Create a function named header which prints numbers from 1 to 10 one by one in `index.html` 
+# and assign to the route of ('/')
+
+# run this app in debug mode on your local.
