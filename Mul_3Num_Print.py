4e1609140b8ef900516095cb9debe30a203e4a76
wgregoryii <90974249+wgregoryii@users.noreply.github.com>
2021-10-21 10:00:29 -0500

    new

---
+++ Mul_3Num_Print.py
@@ -0,0 +1,76 @@
+
+#MULTIPLY-3-Numbers
+#m = input("give me the number you want to multiply: ")
+#y = input("give me the second number you want to multiply: ")
+#z = input("give me a third number you want to multiply: ")
+
+#z = int(z)
+#y = int(y)
+#m = int(m)
+
+#def multiply(numbers):  
+#   total = 1
+#    for x in numbers:
+#        total *= x  
+#    return total  
+#print(multiply((m, z, y)))
+
+#Body Mass Index UK
+#height = float(input("Enter your height in cm: "))
+#weight = float(input("Enter your weight in kg: "))
+
+#BMI = weight / (height/100)**2
+
+#print(f"You BMI is {BMI}")
+
+#if BMI <= 18.4:
+    #print("You are underweight.")
+#elif BMI <= 24.9:
+    #print("You are healthy.")
+#elif BMI <= 29.9:
+    #print("You are over weight.")
+#elif BMI <= 34.9:
+    #print("You are severely over weight.")
+#elif BMI <= 39.9:
+    #print("You are obese.")
+#else:
+    #print("You are severely obese.")
+
+#VARIATION OF BODY MASS POUNDS AND INCHES
+#print("Let's see how FAT you are:")
+#height = float(input("height (in inches)? "))
+#weight = float(input("weight (in pounds)? "))
+ 
+#BMI = weight / height** 2 * 703
+ 
+#if BMI <= 18.4:
+#   print("You are underweight.")
+#elif BMI <= 24.9:
+#   print("You are healthy.")
+#elif BMI <= 29.9:
+#   print("You are over weight.")
+#elif BMI <= 34.9:
+#   print("You are severely over weight.")
+#elif BMI <= 39.9:
+#   print("You are obese.")
+#else:
+#   print("You are severely obese.")
+
+# Asking for two numbers and changing the order
+#print("Please enter two numbers and I will replace them! Muhahahahah!")
+#Num_1= float(input("Number One Please: "))
+#Num_2= float(input("Number Two Please: "))
+
+#Num_1, Num_2 = Num_2, Num_1
+#print(Num_1, Num_2)
+#print(Num_2, Num_1)
+
+#ask user for two numbers and then (x-y)*(x+y)
+#print("Enter Two Numbers: ")
+#Num_1 = float(input("First Number: "))
+#Num_2 = float(input("Second Number: "))
+#output= (Num_1 - Num_2)*(Num_1 + Num_2)
+#print(output)
+
+
+
